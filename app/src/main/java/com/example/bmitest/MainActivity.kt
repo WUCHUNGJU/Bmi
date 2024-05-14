cd8a69a3f8b1c01471890f0e4fabcc23cfa29c68
Louis Wu <bmeru.wu@gmail.com>
2024-05-14 13:39:51 +0800

    conslayout onclick

--- app/src/main/java/com/example/bmitest/MainActivity.kt
+++ app/src/main/java/com/example/bmitest/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.bmitest
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bmitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //
+        setContentView(binding.root)//
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+    }
+
+    fun bmi(view:View)
+    {val weight=binding.edWeight.text.toString().toFloat()
+        val height=binding.edHeight.text.toString().toFloat()
+        val bmi=weight/(height*height)
+        Log.d("BMI",bmi.toString())
+
     }
 }
