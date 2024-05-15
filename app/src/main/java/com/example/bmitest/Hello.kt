eaef5d0e535dafb994726fdb9216524917f134d9
Louis Wu <bmeru.wu@gmail.com>
2024-05-15 13:11:53 +0800

    Third add Hello.kt

---
+++ app/src/main/java/com/example/bmitest/Hello.kt
@@ -0,0 +1,21 @@
+package com.example.bmitest
+
+class Hello {
+}
+
+fun main() {
+    println("Hello")
+    var age =20.3F
+
+    println(age.inc())
+    println(age)
+
+    var name="louis"
+    val x = name.get(2)
+    println(x)
+    name= "fuck"
+    println(name)
+    val weight=65.5F
+    val height=1.7F
+
+}
--- app/src/main/java/com/example/bmitest/MainActivity.kt
+++ app/src/main/java/com/example/bmitest/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.bmitest
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -17,6 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater) //
         setContentView(binding.root)//
+
+        binding.help.setOnClickListener{
+            AlertDialog.Builder(this)
+                .setMessage("體重（kg）/身高的平方(m²)")
+                .setTitle("BMI說明")
+                .setPositiveButton("OK", null)
+                //.setNeutralButton("Cancel", null)
+                .show()
+        }
 
 
 
@@ -37,6 +48,15 @@ class MainActivity : AppCompatActivity() {
         val height=binding.edHeight.text.toString().toFloat()
         val bmi=weight/(height*height)
         Log.d("BMI",bmi.toString())
+        Toast.makeText(this,bmi.toString(),Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
+
+
 
     }
 }
